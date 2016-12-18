4d767333e279e2af4bc515634ff71bff497278d1
jh <j@cjh.io>
2016-12-18 18:20:49 +0100

    fix oauth.access pt 2

--- lib/slack/api/oauth.ex
+++ lib/slack/api/oauth.ex
@@ -2,7 +2,8 @@ defmodule Slack.API.OAuth do
 
   def access(%{client_id: _, client_secret: _, code: _} = params) do
     params = Map.to_list(params)
-    Slack.API.get!("oauth.access", Slack.API.headers, [params: params])
+    res = Slack.API.get!("oauth.access", Slack.API.headers, [params: params])
+    res.body
   end
 
 end
