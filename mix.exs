5aefe5646edd53a1d7c29d4ee2ce7415dfcdfd97
jh <j@cjh.io>
2017-03-03 17:38:11 +0100

    add license

--- mix.exs
+++ mix.exs
@@ -10,24 +10,12 @@ defmodule SlackThrottle.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison],
      mod: {SlackThrottle, []},
      env: [api_throttle: 1000, enqueue_sync_timeout: 20000]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:poison, "~> 2.0"},
